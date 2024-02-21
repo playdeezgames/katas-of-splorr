4c2b6d5934b5174a711a6ae28b83927e24b676f1
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-02-21 05:09:09 -0600

    setting the heading

--- seafarers/seafarers.py
+++ seafarers/seafarers.py
@@ -3,3 +3,6 @@ class Ship:
         self.x = 0
         self.y = 0
         self.heading = 0
+
+    def set_heading(self, new_heading):
+        self.heading = new_heading % 32
--- seafarers/test_seafarers.py
+++ seafarers/test_seafarers.py
@@ -12,3 +12,10 @@ def test_ship_should_have_xy():
 def test_ship_should_have_heading():
     sut = seafarers.Ship()
     assert sut.heading == 0
+
+
+@pytest.mark.parametrize("given_heading, expected_heading", [(32, 0), (33, 1), (-1, 31)])
+def test_ship_should_set_heading(given_heading, expected_heading):
+    sut = seafarers.Ship()
+    sut.set_heading(given_heading)
+    assert sut.heading == expected_heading
