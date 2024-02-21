2d937496e83509eaf913e088c65202091641e3df
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-02-21 05:04:59 -0600

    has a heading

--- seafarers/seafarers.py
+++ seafarers/seafarers.py
@@ -2,3 +2,4 @@ class Ship:
     def __init__(self):
         self.x = 0
         self.y = 0
+        self.heading = 0
--- seafarers/test_seafarers.py
+++ seafarers/test_seafarers.py
@@ -4,6 +4,11 @@ import seafarers
 
 
 def test_ship_should_have_xy():
-    actual = seafarers.Ship()
-    assert actual.x == 0
-    assert actual.y == 0
+    sut = seafarers.Ship()
+    assert sut.x == 0
+    assert sut.y == 0
+
+
+def test_ship_should_have_heading():
+    sut = seafarers.Ship()
+    assert sut.heading == 0
