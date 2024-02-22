b4ba1a5963e577df9364c4d656fb6f699da41d82
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-02-22 05:52:59 -0600

    delver kata begins

--- delver/delver.py
+++ delver/delver.py
@@ -1,2 +1,15 @@
+NORTH = 0
+EAST = 1
+SOUTH = 2
+WEST = 3
+
+VALID_DIRECTIONS = [NORTH, EAST, SOUTH, WEST]
+
+
 class Character:
-    pass
+    def __init__(self):
+        self.facing = NORTH
+
+    def set_facing(self, new_facing):
+        if new_facing in VALID_DIRECTIONS:
+            self.facing = new_facing
--- delver/test_delver.py
+++ delver/test_delver.py
@@ -1,6 +1,22 @@
+import pytest
+
 import delver
 
 
-def test_character_exists():
+def test_character_exists_and_initially_faces_north():
     sut = delver.Character()
-    assert sut is not None
+    assert sut.facing == delver.NORTH
+
+
+@pytest.mark.parametrize(
+    "given_facing, expected_facing",
+    [
+        (delver.EAST, delver.EAST),
+        (delver.SOUTH, delver.SOUTH),
+        (delver.WEST, delver.WEST),
+        (99, delver.NORTH),
+    ])
+def test_character_set_facing(given_facing, expected_facing):
+    sut = delver.Character()
+    sut.set_facing(given_facing)
+    assert sut.facing == expected_facing
