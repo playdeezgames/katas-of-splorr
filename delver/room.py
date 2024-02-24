039012569546d8228c1f93455a7b97d8e3c2c01e
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-02-24 04:31:44 -0600

    i can set an exit for a room

--- delver/room.py
+++ delver/room.py
@@ -1,3 +1,12 @@
+import direction
+
+
 class Room:
-    def has_exit(self, direction):
-        return False
+    def __init__(self):
+        self.exits = [False for _ in range(len(direction.VALID_DIRECTIONS))]
+
+    def has_exit(self, exit_direction):
+        return self.exits[exit_direction]
+
+    def set_exit(self, exit_direction, exit_state):
+        self.exits[exit_direction] = exit_state
--- delver/test_room.py
+++ delver/test_room.py
@@ -5,7 +5,16 @@ import direction
 
 def test_room_exists():
     sut = room.Room()
-    assert not sut.has_exit(direction.NORTH)
-    assert not sut.has_exit(direction.EAST)
-    assert not sut.has_exit(direction.SOUTH)
-    assert not sut.has_exit(direction.WEST)
+    assert not sut.has_exit(direction.NORTH), "room has exit to the north, but should not"
+    assert not sut.has_exit(direction.EAST), "room has exit to the east, but should not"
+    assert not sut.has_exit(direction.SOUTH), "room has exit to the south, but should not"
+    assert not sut.has_exit(direction.WEST), "room has exit to the west, but should not"
+
+
+@pytest.mark.parametrize("given_direction", [
+    direction.NORTH,
+])
+def test_room_set_exit(given_direction):
+    sut = room.Room()
+    sut.set_exit(given_direction, True)
+    assert sut.has_exit(given_direction), f"room does not have an exit in direction {given_direction}"
