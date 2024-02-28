a9fd3d106f51c18c35764cb84a555893a853a3a5
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-02-28 05:39:09 -0600

    inventory contains no item

--- delver/inventory.py
+++ delver/inventory.py
@@ -1,3 +1,6 @@
 class Inventory:
     def __init__(self):
         self.items = []
+
+    def contains_item(self, item):
+        pass
--- delver/test_inventory.py
+++ delver/test_inventory.py
@@ -1,4 +1,5 @@
 import inventory
+import item
 
 
 def test_inventory_exists():
@@ -9,3 +10,9 @@ def test_inventory_exists():
 def test_inventory_starts_with_no_items():
     sut = inventory.Inventory()
     assert len(sut.items) == 0
+
+
+def test_inventory_when_empty_does_not_contain_given_item():
+    sut = inventory.Inventory()
+    my_item = item.Item()
+    assert not sut.contains_item(my_item)
