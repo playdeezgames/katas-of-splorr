c338be4bd343d26f6f13547c71accee55212ecf1
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-02-28 04:44:16 -0600

    eliminate dependency on dungeon by character

--- delver/test_character.py
+++ delver/test_character.py
@@ -1,5 +1,4 @@
 import pytest
-import dungeon
 import character
 import direction
 import room
@@ -63,142 +62,11 @@ def test_character_turn_around(given_initial_facing, expected_final_facing):
     assert sut.facing == expected_final_facing
 
 
-def __prep_dungeon_for_move_test(offset_x, offset_y, given_facing):
-    dungeon_columns = offset_x * 2 + 1
-    dungeon_rows = offset_y * 2 + 1
-    my_dungeon = dungeon.Dungeon(dungeon_columns, dungeon_rows)
-    for column in range(dungeon_columns):
-        for row in range(dungeon_rows):
-            my_room = my_dungeon.get_room(column, row)
-            my_room.set_exit(given_facing, True)
-    return my_dungeon
-
-
-@pytest.mark.parametrize("given_facing, steps, expected_delta_x, expected_delta_y", [
-    (direction.NORTH, 1, 0, 1),
-    (direction.SOUTH, 1, 0, -1),
-    (direction.EAST, 1, 1, 0),
-    (direction.WEST, 1, -1, 0),
-    (direction.NORTH, 2, 0, 2),
-    (direction.SOUTH, 2, 0, -2),
-    (direction.EAST, 2, 2, 0),
-    (direction.WEST, 2, -2, 0),
-])
-def test_character_moves_ahead(given_facing, steps, expected_delta_x, expected_delta_y):
-    offset_x = 2
-    offset_y = 2
-    my_dungeon = __prep_dungeon_for_move_test(offset_x, offset_y, given_facing)
-    sut = character.Character(my_dungeon)
-    sut.move_to_xy(offset_x, offset_y)
-    sut.set_facing(given_facing)
-    for step in range(steps):
-        sut.move_ahead()
-    assert sut.facing == given_facing
-    assert sut.x == expected_delta_x + offset_x
-    assert sut.y == expected_delta_y + offset_y
-
-
-@pytest.mark.parametrize("given_facing, steps, expected_delta_x, expected_delta_y", [
-    (direction.NORTH, 1, -1, 0),
-    (direction.NORTH, 2, -2, 0),
-    (direction.SOUTH, 1, 1, 0),
-    (direction.SOUTH, 2, 2, 0),
-    (direction.EAST, 1, 0, 1),
-    (direction.EAST, 2, 0, 2),
-    (direction.WEST, 1, 0, -1),
-    (direction.WEST, 2, 0, -2),
-])
-def test_character_moves_left(given_facing, steps, expected_delta_x, expected_delta_y):
-    offset_x = 2
-    offset_y = 2
-    my_dungeon = __prep_dungeon_for_move_test(offset_x, offset_y, direction.LEFTS[given_facing])
-    sut = character.Character(my_dungeon)
-    sut.move_to_xy(offset_x, offset_y)
-    sut.set_facing(given_facing)
-    for step in range(steps):
-        sut.move_left()
-    assert sut.facing == given_facing
-    assert sut.x == expected_delta_x + offset_x
-    assert sut.y == expected_delta_y + offset_y
-
-
-@pytest.mark.parametrize("given_facing, steps, expected_delta_x, expected_delta_y", [
-    (direction.NORTH, 1, 1, 0),
-    (direction.NORTH, 2, 2, 0),
-    (direction.SOUTH, 1, -1, 0),
-    (direction.SOUTH, 2, -2, 0),
-    (direction.EAST, 1, 0, -1),
-    (direction.EAST, 2, 0, -2),
-    (direction.WEST, 1, 0, 1),
-    (direction.WEST, 2, 0, 2),
-])
-def test_character_moves_right(given_facing, steps, expected_delta_x, expected_delta_y):
-    offset_x = 2
-    offset_y = 2
-    my_dungeon = __prep_dungeon_for_move_test(offset_x, offset_y, direction.RIGHTS[given_facing])
-    sut = character.Character(my_dungeon)
-    sut.move_to_xy(offset_x, offset_y)
-    sut.set_facing(given_facing)
-    for step in range(steps):
-        sut.move_right()
-    assert sut.facing == given_facing
-    assert sut.x == expected_delta_x + offset_x
-    assert sut.y == expected_delta_y + offset_y
-
-
-@pytest.mark.parametrize("given_facing, steps, expected_delta_x, expected_delta_y", [
-    (direction.NORTH, 1, 0, -1),
-    (direction.SOUTH, 1, 0, 1),
-    (direction.EAST, 1, -1, 0),
-    (direction.WEST, 1, 1, 0),
-    (direction.NORTH, 2, 0, -2),
-    (direction.SOUTH, 2, 0, 2),
-    (direction.EAST, 2, -2, 0),
-    (direction.WEST, 2, 2, 0),
-])
-def test_character_moves_back(given_facing, steps, expected_delta_x, expected_delta_y):
-    offset_x = 2
-    offset_y = 2
-    my_dungeon = __prep_dungeon_for_move_test(offset_x, offset_y, direction.OPPOSITES[given_facing])
-    sut = character.Character(my_dungeon)
-    sut.move_to_xy(offset_x, offset_y)
-    sut.set_facing(given_facing)
-    for step in range(steps):
-        sut.move_back()
-    assert sut.facing == given_facing
-    assert sut.x == expected_delta_x + offset_x
-    assert sut.y == expected_delta_y + offset_y
-
-
-def test_characters_must_be_in_dungeon():
-    given_dungeon = dungeon.Dungeon(1, 1)
-    sut = character.Character(given_dungeon)
-    assert sut.dungeon == given_dungeon, "Character must be in a dungeon"
-
-
-@pytest.mark.parametrize("dungeon_columns, dungeon_rows, given_x, given_y, expected_x, expected_y", [
-    (2, 2, 0, 1, 0, 1),
-    (2, 2, 1, 1, 1, 1),
-    (2, 2, 2, 1, 0, 0),
-])
-def test_character_can_move_within_dungeon(dungeon_columns, dungeon_rows, given_x, given_y, expected_x, expected_y):
-    given_dungeon = dungeon.Dungeon(dungeon_columns, dungeon_rows)
-    sut = character.Character(given_dungeon)
-    sut.move_to_xy(given_x, given_y)
-    assert sut.x == expected_x
-    assert sut.y == expected_y
-
-
 def test_character_is_blocked_when_exit_does_not_exist_for_room():
     my_room = room.Room()
     sut = character.Character(None, 0, 0, my_room)
     sut.move_ahead()
     assert sut.get_room() == my_room
-
-
-def test_character_lacks_room_tracking_toggle():
-    sut = character.Character(dungeon.Dungeon(1, 1))
-    assert not sut.track_room
 
 
 def test_character_tracks_room():
